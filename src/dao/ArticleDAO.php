1354a8e74549f7e4d74324d16b0903468c4bf9d2
Romain <rom.dev@live.fr>
2020-07-04 13:46:29 +0200

    [14] View {DONE}

--- src/dao/ArticleDAO.php
+++ src/dao/ArticleDAO.php
@@ -2,25 +2,40 @@
 
 namespace App\src\DAO;
 
+use App\src\model\Article;
 
-class ArticleDAO
+class ArticleDAO extends DAO
 {
+    private function buildObject($row)
+    {
+        $article = new Article();
+        $article->setId($row['id']);
+        $article->setTitle($row['title']);
+        $article->setContent($row['content']);
+        $article->setAuthor($row['author']);
+        $article->setCreatedAt($row['createdAt']);
+        return $article;
+    }
+
     public function getArticles()
     {
-        $db = new DAO();
-        $connection = $db->getConnection();
-        $result = $connection->query('SELECT id, title, content, author, createdAt FROM article ORDER BY id DESC');
-        return $result;
+        $sql = 'SELECT id, title, content, author, createdAt FROM article ORDER BY id DESC';
+        $result = $this->createQuery($sql);
+        $articles = [];
+        foreach ($result as $row){
+            $articleId = $row['id'];
+            $articles[$articleId] = $this->buildObject($row);
+        }
+        $result->closeCursor();
+        return $articles;
     }
 
     public function getArticle($articleId)
     {
-        $db = new DAO();
-        $connection = $db->getConnection();
-        $result = $connection->prepare('SELECT id, title, content, author, createdAt FROM article WHERE id = ?');
-        $result->execute([
-            $articleId
-        ]);
-        return $result;
+        $sql = 'SELECT id, title, content, author, createdAt FROM article WHERE id = ?';
+        $result = $this->createQuery($sql, [$articleId]);
+        $article = $result->fetch();
+        $result->closeCursor();
+        return $this->buildObject($article);
     }
 }
